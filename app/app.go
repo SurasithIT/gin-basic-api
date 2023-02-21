7471b18eaae285d9265e9ce75ee98b4c1a26ff73
Surasith Kaewvikkit <surasith.kae@ascendcorp.com>
2023-02-21 23:48:43 +0700

    wip: add cors and simple Open API

--- app/app.go
+++ app/app.go
@@ -3,7 +3,9 @@ package app
 import (
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,11 +14,14 @@ func Start() {
 
 	router := gin.Default()
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "DELETE", "PUT", "PATCH"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           24 * time.Hour,
+	}))
 
 	group := router.Group(AppConfig.HTTPPrefix)
 	group.GET("/health", func(c *gin.Context) {
